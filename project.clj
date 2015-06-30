09f16823ef03022b601a273b6c7aa2a4e3d10b6b
David RC Dayton <david.rc.dayton@gmail.com>
2015-06-30 11:58:18 -0600

    updated clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [uk.me.g4dpz/predict4java "1.1.3"]]
   :scm {:name "git"
          :url "https://github.com/david-rc-dayton/satellite-clj"})
